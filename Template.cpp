906c73d8d2eb7d139d4e7ae75f336c9194437b53
S.M.A Nahian <smanahian123@gmail.com>
2021-10-04 18:05:21 +0600

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -25,31 +25,31 @@ typedef vector<ulli>            vulli;
 
 
 
-#define inp(n)              int n; cin >> n
-#define Inp(n)              lli n; cin >> n
-#define print(x)            cout << x End
-#define f0(i,n)             for(li i=0; i < (n); i++)
-#define f1(i,n)             for(li i=1; i <= (n); i++)
-#define rep0(i,a,b)         for(li i=a; i < (b); i++)
-#define rep1(i,a,b)         for(li i=a; i <= (b); i++)
-#define pb                  push_back
-#define lcm(a,b)            (a*b/__gcd(a,b))
-#define srt(v)              sort(v.begin(),v.end())
-#define sz(a)               (li)((a).size())
-#define End                 << endl
-#define testIn              inp(test)
-#define tests               for(int testNo=1; testNo <= (test); testNo++)
-#define yes                 cout << "Yes" End
-#define no                  cout << "No" End
-#define Yes                 cout << "YES" End
-#define No                  cout << "NO" End
-#define finish              return 0
-#define Inf                 (int)1e9
-#define INF                 (lli)1e18
-#define EPS                 (ld)1e-9
-#define PI                  (ld)3.1415926535897932384626433832795
-#define MOD                 1e9+7
-#define MXN                 1e5+7
+#define inp(n)                  int n; cin >> n
+#define Inp(n)                  lli n; cin >> n
+#define print(x)                cout << x End
+#define f0(i,n)                 for(li i=0; i < (n); i++)
+#define f1(i,n)                 for(li i=1; i <= (n); i++)
+#define rep0(i,a,b)             for(li i=a; i < (b); i++)
+#define rep1(i,a,b)             for(li i=a; i <= (b); i++)
+#define pb                      push_back
+#define lcm(a,b)                (a*b/__gcd(a,b))
+#define srt(v)                  sort(v.begin(),v.end())
+#define sz(a)                   (li)((a).size())
+#define End                     << endl
+#define testIn                  inp(test)
+#define tests                   for(int testNo=1; testNo <= (test); testNo++)
+#define yes                     cout << "Yes" End
+#define no                      cout << "No" End
+#define Yes                     cout << "YES" End
+#define No                      cout << "NO" End
+#define finish                  return 0
+#define Inf                     (int)1e9
+#define INF                     (lli)1e18
+#define EPS                     (ld)1e-9
+#define PI                      (ld)3.1415926535897932384626433832795
+#define MOD                     1e9+7
+#define MXN                     1e5+7
 
 
 
