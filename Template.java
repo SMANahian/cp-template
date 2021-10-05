425ad497edf43d9b6c15e7962ec630dae66f3e24
S.M.A Nahian <smanahian123@gmail.com>
2021-10-05 20:08:27 +0600

    Create Template.java

---
+++ Template.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+ 
+public class Template {
+        static Scanner sr = new Scanner(System.in);
+        public static void main(String args[])
+        {
+            
+        }
+}
