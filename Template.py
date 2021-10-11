9c02647eea7078d92cac816bae1b3fd348135751
S.M.A Nahian <smanahian123@gmail.com>
2021-10-11 18:30:31 +0600

    Update Template.py

--- Template.py
+++ Template.py
@@ -28,7 +28,40 @@ def allPrimeList(n, primeList):
             primeList.append(p)
  
 
+def val(c):
+    if c >= '0' and c <= '9':
+        return ord(c) - ord('0')
+    else:
+        return ord(c) - ord('A') + 10
 
+def toDeci(str,base):
+    llen = len(str)
+    power = 1 
+    num = 0 
+ 
+    for i in range(llen - 1, -1, -1):
+        if val(str[i]) >= base:
+            print('Invalid Number')
+            return -1
+        num += val(str[i]) * power
+        power = power * base
+    return num
+
+def reVal(num):
+    if (num >= 0 and num <= 9):
+        return chr(num + ord('0'));
+    else:
+        return chr(num - 10 + ord('A'));
+
+def fromDeci(inputNum, base):
+    res = ""
+    index = 0; 
+
+    while (inputNum > 0):
+        res += reVal(inputNum % base)
+        inputNum = int(inputNum / base)
+
+    return res[::-1]
 
 
 def main():
@@ -37,7 +70,7 @@ def main():
 
     test = int(input())
     for testNo in range(1, test + 1):
-        n = int(input())
+        pass
 
 
 
