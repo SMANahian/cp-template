2c4fd4912557a98c57ecea59904fd14c632d4b66
S.M.A Nahian <smanahian123@gmail.com>
2021-10-29 20:20:06 +0600

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -1,5 +1,76 @@
-#include <bits\stdc++.h>
-using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <bitset>
+#include <ctime>
+
+
+
+
+using std::min;
+using std::max;
+using std::sort;
+using std::to_string;
+using std::cin;
+using std::cout;
+using std::string;
+using std::vector;
+
+
+
+
+typedef long double                 ld;
+typedef unsigned int                ui;
+typedef long int                    li;
+typedef unsigned long int           uli;
+typedef long long int               lli;
+typedef unsigned long long int      ulli;
+typedef vector<int>                 vi;
+typedef vector<ld>                  vld;
+typedef vector<ui>                  vui;
+typedef vector<li>                  vli;
+typedef vector<uli>                 vuli;
+typedef vector<lli>                 vlli;
+typedef vector<ulli>                vulli;
+
+
+
+
+#define inp(n)                      int n; cin >> n
+#define Inp(n)                      lli n; cin >> n
+#define print(x)                    cout << x End
+#define f0(i,n)                     for(li i=0; i < (n); i++)
+#define f1(i,n)                     for(li i=1; i <= (n); i++)
+#define rep0(i,a,b)                 for(li i=a; i < (b); i++)
+#define rep1(i,a,b)                 for(li i=a; i <= (b); i++)
+#define pb                          push_back
+#define lcm(a,b)                    ((a*b)/gcd(a,b))
+#define all(x)                      x.begin(), x.end()
+#define rall(x)                     x.rbegin(), x.rend()
+#define asrt(v)                     sort(all(v))
+#define dsrt(v)                     sort(rall(v))
+#define revStr(str)                 string(rall(str))
+#define sz(a)                       ((a).size())
+#define End                         << "\n"
+#define testIn                      cin >> test
+#define tests                       for(int testNo=1; testNo <= (test); testNo++)
+#define yes                         cout << "yes" End
+#define no                          cout << "no" End
+#define Yes                         cout << "Yes" End
+#define No                          cout << "No" End
+#define YES                         cout << "YES" End
+#define NO                          cout << "NO" End
+#define finish                      return 0
+#define Inf                         (li)(1e9)
+#define INF                         (lli)(1e18)
+#define EPS                         (ld)(1e-9)
+#define PI                          (ld)(3.141592653589793238462643383279502884197169)
+#define MOD                         (li)(1e9+7)
+#define MXN                         (int)(1e5+7)
 
 
 
@@ -13,8 +84,6 @@ dataType gcd(dataType a, dataType b) {
     }
     return a;
 }
-
-
 
 
 template<typename dataType>
@@ -44,9 +113,6 @@ void allPrimeVector(dataType1 n, dataType2 &primeList) {
         if (prime[p])
             primeList.pb(p);
 }
-
-
-
 
 
 template<typename dataType>
@@ -81,6 +147,7 @@ dataType nthBaseToDecimal(string str, dataType base) {
 
     return num;
 }
+
 template<typename dataType>
 char reVal(dataType num) {
     if (num >= 0 && num <= 9)
@@ -97,6 +164,7 @@ string nthBasefromDeci(dataType inputNum, dataType base) {
     }
     return revStr(res);
 }
+
 
 template<typename dataType1, typename dataType2>
 dataType1 smallFactor(dataType1 n, dataType2 &primes) {
@@ -116,7 +184,39 @@ dataType1 smallFactor(dataType1 n, dataType2 &primes) {
 
 
 
-int main() {
+void solve1() {
 
-    return 0;
+}
+
+void solve2() {
+
+}
+
+void solve3() {
+
+}
+
+
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    // Previous code
+
+    int test = 1;
+
+    testIn;
+    
+    tests {
+
+        
+
+    }
+
+    finish;
 }
